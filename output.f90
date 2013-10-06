496e18eb85c164ac34dde6a789024eaef24a1b2f
takuya4880 <takuya4880@gmail.com>
2013-10-07 01:00:46 +0900

    implement of 1d output

--- output.f90
+++ output.f90
@@ -7,12 +7,19 @@ subroutine output(box)
     double precision :: v2(ix,iz), dt(ix,iz)
     m = box%con%marg
 
+    !do j=1,iz
+    !    do i=1,ix
+    !        write(23, *)  box%x(i), box%z(j), box%ro(i,j), box%pr(i,j), box%rovx(i,j), box%rovz(i,j), box%bpot(i,j)
+    !    end do
+    !    write(23,*) " "
+    !end do
+    !write(23,*) " "
+
+    i=100
     do j=1,iz
-        do i=1,ix
-            write(23, *)  box%x(i), box%z(j), box%ro(i,j), box%pr(i,j), box%rovx(i,j), box%rovz(i,j), box%bpot(i,j)
-        end do
-        write(23,*) " "
+        write(23, *)  box%z(j), box%ro(i,j), box%pr(i,j), box%rovx(i,j), box%rovz(i,j), box%bpot(i,j)
     end do
+    write(23,*) " "
     write(23,*) " "
 
 end subroutine
