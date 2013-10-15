1b6cdaa87db61efd82f9e1ab01e9890b8cb9cfa0
takuya4880 <takuya4880@gmail.com>
2013-10-15 19:43:14 +0900

    implementation of parallel calculation to detdt

--- detdt.f90
+++ detdt.f90
@@ -4,6 +4,7 @@ implicit none
 contains
 subroutine detdt(box)
     use defstruct
+    !$use omp_lib
     implicit none
     type(cell) :: box
 
@@ -17,10 +18,11 @@ subroutine detdt(box)
         d = box%con%dz
     end if
 
-
+    !$omp parallel workshare
     v2 = (box%rovx**2 + box%rovy**2 + box%rovz**2)/(box%ro**2)
     v2 = v2 + (box%bx**2 + box%bz**2 + box%bz**2)/box%ro
     v2 = v2 + box%con%gam * box%pr / box%ro
+    !$omp end parallel workshare
 
     box%con%dt = box%con%a * d / sqrt(maxval(v2))
     deallocate(v2)
